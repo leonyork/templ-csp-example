a20316ee9c9458e5f9037e731735808e5cafd6fa
Leon York <leon@leonyork.com>
2023-10-07 21:21:54 +0100

    fmt

--- main.go
+++ main.go
@@ -11,14 +11,14 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handlers.ServeHTTP)
-	
+
 	server := &http.Server{
 		Addr:         "localhost:3000",
 		Handler:      mux,
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 10,
 	}
-	
+
 	fmt.Printf("Server starting on http://%s", server.Addr)
 	server.ListenAndServe()
 }
